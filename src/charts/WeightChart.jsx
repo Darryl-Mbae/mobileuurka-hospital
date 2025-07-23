66756ac1ed30db98481505abf126ebc63a449cd5
Darryl Mbae <darrylmbae01@gmail.com>
2025-07-23 11:33:59 +0300

    fixing env

--- src/charts/WeightChart.jsx
+++ src/charts/WeightChart.jsx
@@ -84,6 +84,7 @@ const WeightChart = ({ data }) => {
         height: "90%", 
         paddingTop: "10px", 
         position: "relative",
+        zIndex:99999,
         overflow: "visible" // Allow tooltip to show outside container
       }} 
       ref={chartRef}
